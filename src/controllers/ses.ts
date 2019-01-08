9db61fd3d342278dbbf28031048aacab7659c554
Thang Vu <ducthang310@gmail.com>
2019-01-08 21:03:49 +0700

    Config for deploying; Define functions, apis; Update mock controller

--- src/controllers/ses.ts
+++ src/controllers/ses.ts
@@ -1,99 +1,19 @@
 import { successResponse } from '../utils';
 
 /**
- * Get listing of the resource
+ * Listens to events from SES's s3 box, parses emails, and puts them into the system.
  *
  * @param event
  * @param _context
  * @param callback
  */
-export const index: AWSLambda.Handler = (
+export const handle: AWSLambda.Handler = (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
   const response = successResponse({
-    users: [],
-    input: event
-  });
-
-  callback(null, response);
-};
-
-/**
- * Get the specified resource in database.
- *
- * @param event
- * @param _context
- * @param callback
- */
-export const load: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
-  _context,
-  callback,
-) => {
-  const response = successResponse({
-    user: {},
-    input: event
-  });
-
-  callback(null, response);
-};
-
-/**
- * Store a newly created resource in database
- *
- * @param event
- * @param _context
- * @param callback
- */
-export const store: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
-  _context,
-  callback,
-) => {
-  const response = successResponse({
-    user: {},
-    input: event
-  });
-
-  callback(null, response);
-};
-
-/**
- * Update the specified resource in database.
- *
- * @param event
- * @param _context
- * @param callback
- */
-export const update: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
-  _context,
-  callback,
-) => {
-  const response = successResponse({
-    user: {},
-    input: event
-  });
-
-  callback(null, response);
-};
-
-/**
- * Delete the specified resource in database.
- *
- * @param event
- * @param _context
- * @param callback
- */
-export const deleteResource: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
-  _context,
-  callback,
-) => {
-  const response = successResponse({
-    user: {},
+    message: 'Parse successfully',
     input: event
   });
 
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,19 +1,99 @@
 import { successResponse } from '../utils';
 
 /**
- * Listens to events from SES's s3 box, parses emails, and puts them into the system.
+ * Get listing of the resource
  *
  * @param event
  * @param _context
  * @param callback
  */
-export const handle: AWSLambda.Handler = (
+export const index: AWSLambda.Handler = (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
   const response = successResponse({
-    message: 'Parse successfully',
+    users: [],
+    input: event
+  });
+
+  callback(null, response);
+};
+
+/**
+ * Get the specified resource in database.
+ *
+ * @param event
+ * @param _context
+ * @param callback
+ */
+export const load: AWSLambda.Handler = (
+  event: AWSLambda.APIGatewayEvent,
+  _context,
+  callback,
+) => {
+  const response = successResponse({
+    user: {},
+    input: event
+  });
+
+  callback(null, response);
+};
+
+/**
+ * Store a newly created resource in database
+ *
+ * @param event
+ * @param _context
+ * @param callback
+ */
+export const store: AWSLambda.Handler = (
+  event: AWSLambda.APIGatewayEvent,
+  _context,
+  callback,
+) => {
+  const response = successResponse({
+    user: {},
+    input: event
+  });
+
+  callback(null, response);
+};
+
+/**
+ * Update the specified resource in database.
+ *
+ * @param event
+ * @param _context
+ * @param callback
+ */
+export const update: AWSLambda.Handler = (
+  event: AWSLambda.APIGatewayEvent,
+  _context,
+  callback,
+) => {
+  const response = successResponse({
+    user: {},
+    input: event
+  });
+
+  callback(null, response);
+};
+
+/**
+ * Delete the specified resource in database.
+ *
+ * @param event
+ * @param _context
+ * @param callback
+ */
+export const deleteResource: AWSLambda.Handler = (
+  event: AWSLambda.APIGatewayEvent,
+  _context,
+  callback,
+) => {
+  const response = successResponse({
+    user: {},
     input: event
   });
 
