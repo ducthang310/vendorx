c09cc155b177f8a5853bd545e6cbc1eec773bd0b
Thang Vu <ducthang310@gmail.com>
2019-01-09 22:12:49 +0700

    Product: list, create, load, update, delete features

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,20 +1,22 @@
-import { successResponse } from '../utils';
+import { errorResponse, successResponse } from '../utils';
+import Product from '../models/Product';
+import moment from 'moment';
 
 /**
  * Get listing of the resource
  *
- * @param event
+ * @param _event
  * @param _context
  * @param callback
  */
-export const index: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
+export const index: AWSLambda.Handler = async (
+  _event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
+  const products = await Product.fetchAll();
   const response = successResponse({
-    products: [],
-    input: event
+    products
   });
 
   callback(null, response);
@@ -27,15 +29,32 @@ export const index: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const load: AWSLambda.Handler = (
+export const load: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    product: {},
-    input: event
-  });
+  let response;
+  // Validate request
+  if (!event || !event.pathParameters || !event.pathParameters.id) {
+    response = errorResponse({
+      message: 'The id field is required'
+    });
+  } else {
+    // Load product
+    let product;
+    const productId = event.pathParameters.id;
+    try {
+      product = await new Product().where('id', productId).fetch();
+      response = successResponse({
+        product
+      });
+    } catch (e) {
+      response = errorResponse({
+        message: 'The product does not exist'
+      });
+    }
+  }
 
   callback(null, response);
 };
@@ -47,15 +66,46 @@ export const load: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const store: AWSLambda.Handler = (
+export const store: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    product: {},
-    input: event
-  });
+  let response;
+  let body;
+  try {
+    body = event.body ? JSON.parse(event.body) : null;
+    let existedProduct;
+    try {
+      existedProduct = await new Product().where('sku', body.sku).fetch();
+    } catch (e) {
+      console.log(e);
+    }
+
+    if (existedProduct) {
+      response = errorResponse({
+        message: 'The product has already existed'
+      });
+    } else {
+      const data = {
+        name: body.name,
+        sku: body.sku,
+        price: body.price,
+        created_at: moment().format('YYYY MM DD HH:mm:ss'),
+        updated_at: moment().format('YYYY MM DD HH:mm:ss')
+      };
+      const newProduct = await new Product(data).save(undefined, {method: 'insert'});
+
+      response = successResponse({
+        product: newProduct,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    response = errorResponse({
+      error: 'Parameters is invalid'
+    });
+  }
 
   callback(null, response);
 };
@@ -67,15 +117,57 @@ export const store: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const update: AWSLambda.Handler = (
+export const update: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    product: {},
-    input: event
-  });
+  let response;
+  // Validate request
+  if (!event || !event.pathParameters || !event.pathParameters.id) {
+    response = errorResponse({
+      message: 'The id field is required'
+    });
+  } else {
+    // Load product
+    let product;
+    const productId = event.pathParameters.id;
+    try {
+      product = await new Product().where('id', productId).fetch();
+
+      try {
+        const body = event.body ? JSON.parse(event.body) : null;
+
+        if (!!body.name || !!body.sku || !!body.price) {
+          const data = {
+            name: !!body.name ? body.name : undefined,
+            sku: !!body.sku ? body.sku : undefined,
+            price: !!body.price ? body.price : undefined
+          };
+
+          await new Product()
+            .where('id', productId)
+            .save(data, {method: 'update', patch: true});
+
+          response = successResponse({
+            product
+          });
+        } else {
+          response = errorResponse({
+            message: 'No data for updating'
+          });
+        }
+      } catch (e) {
+        response = errorResponse({
+          error: e
+        });
+      }
+    } catch (e) {
+      response = errorResponse({
+        message: 'The product does not exist'
+      });
+    }
+  }
 
   callback(null, response);
 };
@@ -87,15 +179,31 @@ export const update: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const deleteResource: AWSLambda.Handler = (
+export const deleteResource: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    product: {},
-    input: event
-  });
+  let response;
+  // Validate request
+  if (!event || !event.pathParameters || !event.pathParameters.id) {
+    response = errorResponse({
+      message: 'The id field is required'
+    });
+  } else {
+    const productId = event.pathParameters.id;
+    try {
+      await new Product().where('id', productId).destroy();
+
+      response = successResponse({
+        message: 'Deleted successfully'
+      });
+    } catch (e) {
+      response = errorResponse({
+        message: 'The product does not exist'
+      });
+    }
+  }
 
   callback(null, response);
 };
