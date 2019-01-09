7142dcd829cc101ec1fc0ba76ebc3cb8f1a1e3b5
Thang Vu <ducthang310@gmail.com>
2019-01-09 22:52:42 +0700

    Transaction: list, create, load, update, delete features

--- migrations/20190108214557_create_transactions_table.js
+++ migrations/20190108214557_create_transactions_table.js
@@ -3,8 +3,9 @@ exports.up = function(knex) {
   return knex.schema.createTable('transactions', function(t) {
     t.increments('id').unsigned().primary();
     t.integer('customer_id').nullable();
-    t.integer('created_by').nullable();
+    t.string('created_by').nullable();
     t.string('type').nullable();
+    t.string('source').nullable();
     t.timestamp('created_at').notNull();
     t.timestamp('updated_at').nullable();
   });
--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -1,20 +1,22 @@
-import { successResponse } from '../utils';
+import { errorResponse, successResponse } from '../utils';
+import Transaction from '../models/Transaction';
+import moment from 'moment';
 
 /**
  * Get listing of the resource
  *
- * @param event
+ * @param _event
  * @param _context
  * @param callback
  */
-export const index: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
+export const index: AWSLambda.Handler = async (
+  _event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
+  const transactions = await Transaction.fetchAll();
   const response = successResponse({
-    transactions: [],
-    input: event
+    transactions
   });
 
   callback(null, response);
@@ -27,15 +29,32 @@ export const index: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const load: AWSLambda.Handler = (
+export const load: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    transaction: {},
-    input: event
-  });
+  let response;
+  // Validate request
+  if (!event || !event.pathParameters || !event.pathParameters.id) {
+    response = errorResponse({
+      message: 'The id field is required'
+    });
+  } else {
+    // Load transaction
+    let transaction;
+    const transactionId = event.pathParameters.id;
+    try {
+      transaction = await new Transaction().where('id', transactionId).fetch();
+      response = successResponse({
+        transaction
+      });
+    } catch (e) {
+      response = errorResponse({
+        message: 'The transaction does not exist'
+      });
+    }
+  }
 
   callback(null, response);
 };
@@ -47,15 +66,41 @@ export const load: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const store: AWSLambda.Handler = (
+export const store: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    transaction: {},
-    input: event
-  });
+  let response;
+  let body;
+  try {
+    body = event.body ? JSON.parse(event.body) : null;
+
+    if (!body.type || !body.source) {
+      response = errorResponse({
+        message: 'Parameters must contain: type, source'
+      });
+    } else {
+      const data = {
+        customer_id: body.customer_id,
+        created_by: body.created_by,
+        type: body.type,
+        source: body.source,
+        created_at: moment().format('YYYY MM DD HH:mm:ss'),
+        updated_at: moment().format('YYYY MM DD HH:mm:ss')
+      };
+      const newTransaction = await new Transaction(data).save(undefined, {method: 'insert'});
+
+      response = successResponse({
+        transaction: newTransaction,
+      });
+    }
+  } catch (e) {
+    console.log(e)
+    response = errorResponse({
+      message: 'Parameters is invalid'
+    });
+  }
 
   callback(null, response);
 };
@@ -67,15 +112,58 @@ export const store: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const update: AWSLambda.Handler = (
+export const update: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    transaction: {},
-    input: event
-  });
+  let response;
+  // Validate request
+  if (!event || !event.pathParameters || !event.pathParameters.id) {
+    response = errorResponse({
+      message: 'The id field is required'
+    });
+  } else {
+    // Load transaction
+    let transaction;
+    const transactionId = event.pathParameters.id;
+    try {
+      transaction = await new Transaction().where('id', transactionId).fetch();
+
+      try {
+        const body = event.body ? JSON.parse(event.body) : null;
+
+        if (!!body.customer_id || !!body.created_by || !!body.type || !!body.source) {
+          const data = {
+            customer_id: !!body.customer_id ? body.customer_id : undefined,
+            created_by: !!body.created_by ? body.created_by : undefined,
+            type: !!body.type ? body.type : undefined,
+            source: !!body.source ? body.source : undefined
+          };
+
+          await new Transaction()
+            .where('id', transactionId)
+            .save(data, {method: 'update', patch: true});
+
+          response = successResponse({
+            transaction
+          });
+        } else {
+          response = errorResponse({
+            message: 'No data for updating'
+          });
+        }
+      } catch (e) {
+        response = errorResponse({
+          error: e
+        });
+      }
+    } catch (e) {
+      response = errorResponse({
+        message: 'The transaction does not exist'
+      });
+    }
+  }
 
   callback(null, response);
 };
@@ -87,15 +175,31 @@ export const update: AWSLambda.Handler = (
  * @param _context
  * @param callback
  */
-export const deleteResource: AWSLambda.Handler = (
+export const deleteResource: AWSLambda.Handler = async (
   event: AWSLambda.APIGatewayEvent,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    transaction: {},
-    input: event
-  });
+  let response;
+  // Validate request
+  if (!event || !event.pathParameters || !event.pathParameters.id) {
+    response = errorResponse({
+      message: 'The id field is required'
+    });
+  } else {
+    const transactionId = event.pathParameters.id;
+    try {
+      await new Transaction().where('id', transactionId).destroy();
+
+      response = successResponse({
+        message: 'Deleted successfully'
+      });
+    } catch (e) {
+      response = errorResponse({
+        message: 'The transaction does not exist'
+      });
+    }
+  }
 
   callback(null, response);
 };
