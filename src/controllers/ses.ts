c44987e52faec1280231d4b68a865541ca85b839
Thang Vu <ducthang310@gmail.com>
2019-01-13 12:18:23 +0700

    Function for process incoming email

--- src/controllers/ses.ts
+++ src/controllers/ses.ts
@@ -1,4 +1,11 @@
-import { successResponse } from '../utils';
+// import { S3EventRecord } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+import { parseEmailFile } from '../utils/mail';
+// import * as stream from 'stream';
+// import { ParsedMail } from 'mailparser';
+// import moment from 'moment';
+// import Transaction from '../models/Transaction';
+// import { successResponse } from '../utils';
 
 /**
  * Listens to events from SES's s3 box, parses emails, and puts them into the system.
@@ -8,14 +15,52 @@ import { successResponse } from '../utils';
  * @param callback
  */
 export const handle: AWSLambda.Handler = (
-  event: AWSLambda.APIGatewayEvent,
+  event: AWSLambda.S3Event,
   _context,
   callback,
 ) => {
-  const response = successResponse({
-    message: 'Parse successfully',
-    input: event
-  });
+  const records = event.Records;
 
-  callback(null, response);
+  records.forEach(record => {
+    console.log(`${record.s3.bucket.name} - ${record.s3.object.key}`);
+
+    const {
+      s3: {
+        bucket: { name: bucketName },
+        object: { key: objectKey },
+      },
+    } = record
+
+    const s3 = new S3();
+
+    const getEmailFile = new Promise((resolve, reject) => {
+      try {
+        const emailFile = s3
+          .getObject({
+            Bucket: bucketName,
+            Key: objectKey,
+          })
+          .createReadStream()
+
+        resolve(emailFile)
+      } catch (error) {
+        console.log('error getting email file')
+        reject(error)
+      }
+    });
+
+    console.log('getting email file')
+    getEmailFile
+      .then(parseEmailFile)
+      .then(e => {
+        console.log('email file parsed')
+        console.log(e);
+
+      })
+      .catch(error => {
+        console.error(error);
+        callback(error)
+      })
+  })
+
 };
